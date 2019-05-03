5416f9a09974bb3b64a2588845eae40959061144
Victor Rodriguez <vical.rl@gmail.com>
2019-05-03 18:01:24 -0400

    Test

--- src/components/Link.js
+++ src/components/Link.js
@@ -12,9 +12,11 @@ const Link = ({ active, children, onClick }) => (
 )
 
 Link.propTypes = {
+
   active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired
+
 }
 
 export default Link
